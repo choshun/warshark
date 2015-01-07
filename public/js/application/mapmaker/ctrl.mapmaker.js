90d5f87ccadc681e3eb9043fa406f478b0f8aeba
Choshun Snyder <csnyder@hugeinc.com>
2015-01-07 13:24:03 -0500

    adding update, and timeout for directive to model testing

--- public/js/application/mapmaker/ctrl.mapmaker.js
+++ public/js/application/mapmaker/ctrl.mapmaker.js
@@ -1,8 +1,29 @@
 angular
     .module('mapmaker')
-    .controller('MapmakerCtrl', function() {
+    .controller('MapmakerCtrl', ['$scope', '$timeout', function($scope, $timeout) {
+    	
     	this.model = {
     		'Rob': 'I am the captian now',
-    		'I believe in alex': false
+    		'I believe in alex': false,
+    		'nodes': [
+    			{
+    				'name': 'Mexico'
+    			},
+    			{
+    				'name': 'Fresno'
+    			}
+    		]
     	};
-    });
+
+    	this.updateNodeName = function(name, index) {
+    		this.model.nodes[index].name = name;
+    		$scope.$apply();
+    		console.log(this.model);
+    	};
+
+    	// For testing directive firing when control model changes with $observe
+		$timeout(angular.bind(this, function() {
+			this.model.nodes[0].name = 'snyarf';
+			$scope.$apply();
+		}), 3000);
+    }]);
